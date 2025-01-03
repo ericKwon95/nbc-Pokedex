0e6cb93802d01ba9dd182dc6a5c86b81f9f32110
ericKwon95 <ericyong95@gmail.com>
2025-01-03 17:16:22 +0900

    feat: 컬렉션뷰 셀 구현

---
+++ Pokedex/View/Main/PokemonThumbnailCell.swift
@@ -0,0 +1,50 @@
+//
+//  PokemonThumbnailCell.swift
+//  Pokedex
+//
+//  Created by 권승용 on 1/2/25.
+//
+
+import UIKit
+import SnapKit
+
+final class PokemonThumbnailCell: UICollectionViewCell {
+    static let identifier = "PokemonThumbnailCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+    }
+    
+    private func configureUI() {
+        backgroundColor = .cellBackground
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        
+        [
+            imageView
+        ].forEach { contentView.addSubview($0) }
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func configureCell(with url: URL) {
+        imageView.setImage(from: url)
+    }
+}
